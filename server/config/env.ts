f78980c5ed1c93b5f4c602fd7cdab5d5e02f2a9b
LangkaWS <60548795+LangkaWS@users.noreply.github.com>
2021-09-30 15:58:23 +0200

    ✨create env.ts
    env.ts get, parse and check .env before returning environment variables

---
+++ server/config/env.ts
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv';
+
+const getVars = (): dotenv.DotenvParseOutput => {
+  const dotenvConfig = dotenv.config({ path: `${__dirname}/.env`});
+  if (dotenvConfig.error) {
+    throw dotenvConfig.error;
+  }
+
+  const envVars = dotenvConfig.parsed;
+  if (!envVars) {
+    throw new Error('No environment variables defined. Process terminated.');
+  }
+
+  return envVars;
+}
+
+const verifyVars = (props: Array<string>, vars: dotenv.DotenvParseOutput): boolean => {
+  return props.every(prop => prop in vars);
+}
+
+export default () => {
+  try {
+    const envVars = getVars();
+    const reqProps = ['SECRET', 'DB_DIALECT', 'DB_HOST', 'DB_USER', 'DB_PASSWORD', 'DB_NAME'];
+    
+    if (verifyVars(reqProps, envVars)) {
+      return envVars;
+    } else {
+      throw new Error('Missing environment variables. Process terminated.');
+    }
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
