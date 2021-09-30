1128014c2f0c77bfbf252070ae5d5b43671a1e7a
LangkaWS <60548795+LangkaWS@users.noreply.github.com>
2021-09-30 15:58:52 +0200

    ✨create server.ts

---
+++ server/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import env from './config/env';
+import { createServer } from 'http';
+
+const envVars = env();
+
+const app = express();
+const port = envVars.PORT || 8800;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const httpServer = createServer(app);
+
+httpServer.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
