e2e2d4abebdd6d9ff6b5881c314d152f85c9f4bc
LangkaWS <60548795+LangkaWS@users.noreply.github.com>
2021-10-08 13:02:51 +0200

    ✨ add 404 response to unmatched routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,11 +1,15 @@
-import { Application } from 'express';
+import { Application, Request, Response } from 'express';
 import home from './home';
 import user from './user/user';
 import { verifyToken } from '../auth';
+import { ApiResponse } from '../api/ApiResponse';
 
 const config = (app: Application): void => {
 	app.use('/users', verifyToken, user);
 	app.use('/', home);
+	app.use('*', (req: Request, res: Response) => {
+		res.status(404).send(new ApiResponse(false, null, 'PageNotFound'));
+	});
 };
 
 export default config;
