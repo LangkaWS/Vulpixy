e89d8e3f57a51def6e226bba9fea5303e05966a4
LangkaWS <60548795+LangkaWS@users.noreply.github.com>
2021-10-15 18:09:08 +0200

    🐛 add material button module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ErrorDialogComponent } from './errors/error-dialog/error-dialog.compone
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GlobalErrorHandler } from './_services/error/global-error-handler';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { ProfileComponent } from './user/profile/profile.component';
 
@@ -32,7 +33,8 @@ import { ProfileComponent } from './user/profile/profile.component';
 		ReactiveFormsModule,
 		HttpClientModule,
 		BrowserAnimationsModule,
-		MatDialogModule
+		MatDialogModule,
+		MatButtonModule
   ],
   providers: [
 		{
